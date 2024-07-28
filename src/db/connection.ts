10fc365ad876d713fcc896e0125f88ec162768db
Sagar-v4 <sagarvariya4@gmail.com>
2024-07-29 00:25:20 +0530

    feat: shared lib init

---
+++ src/db/connection.ts
@@ -0,0 +1,16 @@
+import { ModelDefinition } from "@nestjs/mongoose";
+
+// import { CONNECTION } from "@/common/db/mongo/connection";
+
+export interface MongooseDbSchema {
+  connectionName: string;
+  models: ModelDefinition[];
+}
+
+export enum MONGOOSE_DB_CONNECTION {}
+// MAIN = CONNECTION.RELAY_SERVICE_MAIN,
+
+export const MONGOOSE_DB_SCHEMA = {};
+
+export enum REDIS_DB_CONNECTION {}
+// MAIN = CONNECTION.RELAY_SERVICE_MAIN,
